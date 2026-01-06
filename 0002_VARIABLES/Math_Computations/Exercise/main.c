c632ad9aa2b2be71fdf9dc5c8d015b450e08eac3
marieelsdhbw <inf25052@lehre.dhbw-stuttgart.de>
2026-01-06 21:11:50 +0100

    0002 Variables

--- 0002_VARIABLES/Math_Computations/Exercise/main.c
+++ 0002_VARIABLES/Math_Computations/Exercise/main.c
@@ -1,12 +1,36 @@
 #include <stdio.h>
 
+
+
+
 int main()
 {
 
+    int x, y;
+
+    printf("enter two numbers:");
+    scanf("%d %d", &x,&y);
+
+    //if the second input is 0 it is an error
+    if (y==0)
+    {
+        printf("error: division by zero\n");
+        return 1;
+    }
+
+
+    //the math problems:
+    float z_1 = x*x + y*y - (x * y) + 2;
+    float z_2 = (x - y)*(x - y)*(x - y) - 3;
+    float z_3 = (2*x*x*x-x*x/2.0-x+4)/y;
+
+
+    printf("z_1: %f\n", z_1);
+    printf("z_2: %f\n", z_2);
+    printf("z_3: %f\n", z_3);
+
     return 0;
 }
-
-
 
 
 
